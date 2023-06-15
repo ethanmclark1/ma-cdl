d239b343993d849c52d1d5e614433e256822643d
Ethan <eclark715@gmail.com>
2023-06-15 11:34:50 -0700

    Updated speaker directions

--- ma-cdl/agents/speaker.py
+++ ma-cdl/agents/speaker.py
@@ -2,9 +2,13 @@ from shapely import Point
 from agents.utils.a_star import a_star
 
 class Speaker:
-    def __init__(self, num_agents):
+    def __init__(self, num_agents, agent_radius, goal_radius, obstacle_radius):
         self.num_agents = num_agents
-        
+        self.agent_radius = agent_radius
+        self.goal_radius = goal_radius
+        self.obstacle_radius = obstacle_radius
+    
+    # TODO: Account for agent size
     def localize(self, pos, language):
         try:
             region_idx = list(map(lambda region: region.contains(pos), language)).index(True)
@@ -16,15 +20,16 @@ class Speaker:
     def direct(self, state, language):
         directions = []
         
-        obs_pos = state[-self.num_agents*2:-self.num_agents*2+2]
-        obstacles = [Point(obs) for obs in obs_pos]
+        obstacle_positions = state[self.num_agents*2 : -self.num_agents*2].reshape(-1, 2)
+        obstacles = [Point(obstacle_position).buffer(self.obstacle_radius) 
+                     for obstacle_position in obstacle_positions]
         
         for idx in range(self.num_agents):
-            start_pos = state[idx*2:idx*2+2]
-            start_idx = self.localize(Point(start_pos), language)
-            goal_pos = state
-            goal_idx = self.localize(Point(goal_pos), language)
+            agent = Point(state[idx*2 : idx*2+2]).buffer(self.agent_radius)
+            agent_idx = self.localize(agent, language)
+            goal = Point(state[-self.num_agents*2+idx : -self.num_agents*2+idx+2]).buffer(self.goal_radius)
+            goal_idx = self.localize(goal, language)
             
-            directions += [a_star(start_idx, goal_idx, obstacles, language)]
+            directions += [a_star(agent_idx, goal_idx, obstacles, language)]
                     
         return directions

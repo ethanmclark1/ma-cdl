8f5865a4091f1231a689331c6dcc07e1889018eb
Ethan <eclark715@gmail.com>
2023-06-20 11:10:58 -0700

    Removed unneccesary variables from network.py

--- ma-cdl/languages/utils/networks.py
+++ ma-cdl/languages/utils/networks.py
@@ -53,18 +53,16 @@ class Autoencoder(nn.Module):
     
 
 class Actor(nn.Module):
-    def __init__(self, state_dim, action_dim, max_action):
+    def __init__(self, state_dim, action_dim):
         super(Actor, self).__init__()
         self.l1 = nn.Linear(state_dim, 128)
         self.l2 = nn.Linear(128, 64)
         self.l3 = nn.Linear(64, action_dim)
-        
-        self.max_action = max_action
-    
+            
     def forward(self, x):
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
-        coefficients = self.max_action * torch.tanh(self.l3(x))
+        coefficients = torch.tanh(self.l3(x))
         return coefficients
     
     

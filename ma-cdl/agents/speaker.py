08b123492844f3537ce724d17ddef0597ab3b9ea
Ethan <eclark715@gmail.com>
2023-06-22 14:02:44 -0700

    Streamlined speaker.direct

--- ma-cdl/agents/speaker.py
+++ ma-cdl/agents/speaker.py
@@ -8,14 +8,16 @@ class Speaker:
         self.obstacles = None
         self.num_agents = num_agents
         self.obstacle_radius = obstacle_radius
+        self.languages = ['EA', 'TD3', 'Bandit']
     
     # Determine the positions of the agents, goals, and obstacles
     def gather_info(self, state):
-        self.obstacles = state[self.num_agents*2 : -self.num_agents*2].reshape(-1, 2)
         
         for idx in range(self.num_agents):
             self.agents += [state[idx*2 : idx*2+2]]
-            self.goals += [state[-self.num_agents*2 + idx*2 - 2 : -self.num_agents*2 + idx*2]]
+            self.goals += [state[self.num_agents*2 + idx*2 : self.num_agents*2 + idx*2 + 2]]
+            
+        self.obstacles = state[self.num_agents*4 : ].reshape(-1, 2)
         
     # Find the region that contains the entity
     def localize(self, entity, language):
@@ -25,24 +27,20 @@ class Speaker:
             region_idx = None
         return region_idx    
     
-    # Provide directions for each agent to get to their respective goal using CDL
-    def direct_with_cdl(self, language):
+    def direct(self, approach):
         directions = []
+        class_name = approach.__class__.__name__
         
-        obstacles = [Point(obstacle).buffer(self.obstacle_radius) 
-                     for obstacle in self.obstacles]
-        
-        for agent, goal in zip(self.agents, self.goals):
-            agent_idx = self.localize(Point(agent), language)
-            goal_idx = self.localize(Point(goal), language)
-            directions += [a_star(agent_idx, goal_idx, obstacles, language)]
-                    
-        return directions
-    
-    # Provide directions for each agent to get to their respective goal using baseline
-    def direct_with_baseline(self, baseline):
-        directions = []
-        for agent, goal in zip(self.agents, self.goals):
-            directions += [baseline.direct(agent, goal, self.obstacles)]
-        
+        if class_name in self.langauges:
+            obstacles = [Point(obstacle).buffer(self.obstacle_radius) 
+                         for obstacle in self.obstacles]
+            
+            for agent, goal in zip(self.agents, self.goals):
+                agent_idx = self.localize(Point(agent), approach)
+                goal_idx = self.localize(Point(goal), approach)
+                directions += [a_star(agent_idx, goal_idx, obstacles, approach)]
+        else:
+            for agent, goal in zip(self.agents, self.goals):
+                directions += [approach.direct(agent, goal, self.obstacles)]
+            
         return directions

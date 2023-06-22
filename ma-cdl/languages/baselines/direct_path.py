001094a35d47df58be871abe76de887ce24ff936
Ethan <eclark715@gmail.com>
2023-06-22 12:56:17 -0700

    Implemented direct_path with RRT*

--- ma-cdl/languages/baselines/direct_path.py
+++ ma-cdl/languages/baselines/direct_path.py
@@ -1,3 +1,13 @@
+from languages.utils.rrt_star import RRTStar
+
 class DirectPath:
-    def __init__(self):
-        a=3
+    def __init__(self, agent_radius, goal_radius, obstacle_radius):
+        self.planner = RRTStar(
+            agent_radius,
+            goal_radius,
+            obstacle_radius
+            )
+    
+    def direct(self, start, goal, obstacles):
+        direct_path = self.planner.get_path(start, goal, obstacles)
+        return direct_path

f96edeaf482d7ee2e8f0692f267b9874b7484cf1
Ethan <eclark715@gmail.com>
2023-06-30 13:54:18 -0700

    Built out VoronoiMap baseline

--- ma-cdl/languages/baselines/voronoi_map.py
+++ ma-cdl/languages/baselines/voronoi_map.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-
 import networkx as nx
+
 from languages.utils.cdl import CDL
 from agents.utils.a_star import a_star
 from shapely import Point, MultiPoint, Polygon, MultiPolygon, voronoi_polygons
@@ -12,18 +11,7 @@ class VoronoiMap:
         self.show_animation = show_animation
         self.bbox = Polygon([(-1, -1), (1, -1), (1, 1), (-1, 1)])
     
-    @staticmethod
-    def voronize(position):
-        closest_idx = VoronoiMap.tree.query(position, k=1)[1]
-        return closest_idx
-    
-    @staticmethod
-    def get_centroid(idx):
-        point = VoronoiMap.voronoi_map.points[idx]
-        return point
-    
     # Create the voronoi map using Shapely
-    @staticmethod
     def create_voronoi_map(self, start, goal, obstacles):
         points = MultiPoint([start, goal, *obstacles])
         voronoi_map = voronoi_polygons(points)
@@ -57,33 +45,5 @@ class VoronoiMap:
         except TypeError:
             graph = CDL.create_graph(VoronoiMap.regions)
             directions = nx.shortest_path(graph, agent_idx, goal_idx)
-                    
-        if self.show_animation:
-            print(directions)
-            self.plot(start, goal, obstacles)
 
         return directions
-    
-    def plot(self, start, goal, obstacle):        
-        _, ax = plt.subplots()
-        
-        plt.scatter(start[0], start[1], color='green', marker='s', label='start')
-        plt.scatter(goal[0], goal[1], color='red', marker='*', label='goal')
-        
-        obstacle_x = [p.x for p in obstacle]
-        obstacle_y = [p.y for p in obstacle]
-        plt.scatter(obstacle_x, obstacle_y, color='black', marker='x', label='obstacle')
-        
-        for i, region in enumerate(VoronoiMap.regions):
-            centroid = region.centroid
-            plt.text(centroid.x, centroid.y, str(i), color='black', fontsize=10)
-            polygon = plt.Polygon(region.exterior.coords, color='blue', alpha=0.2)
-            ax.add_patch(polygon)
-        
-        plt.xlim(-1, 1)
-        plt.ylim(-1, 1)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        
-        plt.legend()
-        plt.show()

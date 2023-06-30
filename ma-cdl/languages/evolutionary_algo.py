aa550b490e60e55f5bda160c26a614f2b91784a7
Ethan <eclark715@gmail.com>
2023-06-30 14:16:02 -0700

    Finetuned EA

--- ma-cdl/languages/evolutionary_algo.py
+++ ma-cdl/languages/evolutionary_algo.py
@@ -9,12 +9,18 @@ from languages.utils.cdl import CDL
 class EA(CDL):
     def __init__(self, scenario, world):
         super().__init__(scenario, world)
+        self._init_hyperparams()
+        
+    def _init_hyperparams(self):
+        self.tol = 0.04
+        self.max_iter = 175
         
     def _init_wandb(self):
-        wandb.init(project='ma-cdl', entity='ethanmclark1', name='evolutionary_algo')
+        wandb.init(project='ma-cdl', entity='ethanmclark1', name='EA')
         config = wandb.config
-        config.learning_rate = self.learning_rate
-        config.num_train_epochs = self.num_train_epochs
+        config.tol = self.tol
+        config.weights = self.weights
+        config.max_iter = self.max_iter
         
     # Upload regions to Weights and Biases
     def _log_regions(self, problem_instance, num_lines, coeffs, cost):
@@ -25,7 +31,7 @@ class EA(CDL):
         pil_image = super()._get_image(problem_instance, 'num_lines', num_lines, regions, -cost)
         wandb.log({"image": wandb.Image(pil_image)})
                 
-    def _optimizer(self, coeffs, problem_instance):
+    def optimizer(self, coeffs, problem_instance):
         lines = CDL.get_lines_from_coeffs(coeffs)
         valid_lines = CDL.get_valid_lines(lines)
         regions = CDL.create_regions(valid_lines)
@@ -34,12 +40,14 @@ class EA(CDL):
         
     # Minimizes cost function to generate the optimal lines
     def _generate_optimal_coeffs(self, problem_instance):
+        self._init_wandb()
+        
         lb, ub = -1, 1
         optim_val, optim_coeffs = inf, None
         for num in range(self.min_lines, self.max_lines + 1):
             bounds = [(lb, ub) for _ in range(3*num)]
-            res = optimize.differential_evolution(self._optimizer, bounds, args=(problem_instance,),
-                                                  tol=0.5, maxiter=100*num, init='halton')
+            res = optimize.differential_evolution(self.optimizer, bounds, args=(problem_instance,),
+                                                     tol=self.tol, maxiter=self.max_iter*num, init='sobol')
             wandb.log({'cost': res.fun, 'num_lines': num})
             self._log_regions(problem_instance, num, res.x, res.fun)
 
@@ -47,5 +55,6 @@ class EA(CDL):
                 optim_val = res.fun
                 optim_coeffs = res.x
         
+        wandb.finish()
         optim_coeffs = np.reshape(optim_coeffs, (-1, 3))
         return optim_coeffs

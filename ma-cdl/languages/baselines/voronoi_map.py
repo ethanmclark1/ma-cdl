c9de80f0aec0e48ead4efb3ad8b223b8c50345bf
Ethan <eclark715@gmail.com>
2023-06-30 10:43:42 -0700

    Integrated Shapely into Voronoi Map

--- ma-cdl/languages/baselines/voronoi_map.py
+++ ma-cdl/languages/baselines/voronoi_map.py
@@ -1,61 +1,89 @@
-import numpy as np
-import networkx as nx
 import matplotlib.pyplot as plt
 
-from scipy.spatial import Voronoi, voronoi_plot_2d, KDTree
+import networkx as nx
+from languages.utils.cdl import CDL
+from agents.utils.a_star import a_star
+from shapely import Point, MultiPoint, Polygon, MultiPolygon, voronoi_polygons
 
 class VoronoiMap:
-    def __init__(self, agent_radius, goal_radius, obstacle_radius):
-        self.voronoi = None
-        
-    def _build_graph(self, critical_points):
-        G = nx.Graph()
-        self.voronoi = Voronoi(critical_points)
-        for index, vertex in enumerate(self.voronoi.vertices):
-            G.add_node(index, pos=vertex)
-            
-        for ridge_vertices in self.voronoi.ridge_vertices:
-            if -1 not in ridge_vertices:
-                v1, v2 = ridge_vertices
-                G.add_edge(v1, v2, weight=np.linalg.norm(self.voronoi.vertices[v1] - self.voronoi.vertices[v2]))
-        
-        return G
-
-    # TODO: Is not working correctly
-    def direct(self, start, goal, obstacles):
-        critical_points = np.vstack([start, goal, obstacles])
-        graph = self._build_graph(critical_points)
-        
-        tree = KDTree(self.voronoi.vertices)
-        start_vertex = tree.query(start)[1]
-        goal_vertex = tree.query(goal)[1]
-
-        path = nx.astar_path(graph, start_vertex, goal_vertex, weight='weight')
-        print(path)
-        self.plot(start, goal, obstacles, path)
-
-        return path
+    regions = None
     
-    # TODO: Is not working correctly
-    def plot(self, start, goal, obstacles, directions):
-        fig, ax = plt.subplots()
-        voronoi_plot_2d(self.voronoi, ax=ax, show_vertices=False, line_colors='orange', line_width=2, line_alpha=0.6, point_size=2)
+    def __init__(self, show_animation=True):
+        self.show_animation = show_animation
+        self.bbox = Polygon([(-1, -1), (1, -1), (1, 1), (-1, 1)])
+    
+    @staticmethod
+    def voronize(position):
+        closest_idx = VoronoiMap.tree.query(position, k=1)[1]
+        return closest_idx
+    
+    @staticmethod
+    def get_centroid(idx):
+        point = VoronoiMap.voronoi_map.points[idx]
+        return point
+    
+    # Create the voronoi map using Shapely
+    @staticmethod
+    def create_voronoi_map(self, start, goal, obstacles):
+        points = MultiPoint([start, goal, *obstacles])
+        voronoi_map = voronoi_polygons(points)
+        return voronoi_map
 
-        ax.plot(*start, 'go')
-        ax.plot(*goal, 'ro')
-
-        ax.plot(obstacles[:, 0], obstacles[:, 1], 'ko')
-
-        if directions is not None:
-            path_points = self.voronoi.vertices[directions]
-            ax.plot(path_points[:, 0], path_points[:, 1], 'b-')
+    # Clip the voronoi map to the bounding box
+    def clip_voronoi(self, voronoi_map):
+        clipped_voronoi = []
+        for region in voronoi_map.geoms:
+            clipped_polygon = self.bbox.intersection(region)
+            if clipped_polygon.is_empty:
+                continue
+            elif clipped_polygon.geom_type == 'Polygon':
+                clipped_voronoi.append(clipped_polygon)
+            elif clipped_polygon.geom_type == 'MultiPolygon':
+                clipped_voronoi.extend(clipped_polygon)
+        return MultiPolygon(clipped_voronoi)
+    
+    def direct(self, start, goal, obstacles):   
+        voronoi_map = self.create_voronoi_map(start, goal, obstacles) 
+        clipped_voronoi = self.clip_voronoi(voronoi_map) 
+        VoronoiMap.regions = [*clipped_voronoi.geoms]
         
-        # label Voronoi regions
-        for i, region in enumerate(self.voronoi.regions):
-            if not -1 in region:
-                polygon = self.voronoi.vertices[region]
-                centroid = polygon.mean(axis=0)
-                ax.text(*centroid, str(i), color='blue')
+        agent_idx = CDL.localize(Point(start), VoronoiMap.regions)
+        goal_idx = CDL.localize(Point(goal), VoronoiMap.regions)
+        
+        obstacles = [Point(obstacle) for obstacle in obstacles]
 
-        ax.set_aspect('equal')
+        try:
+            directions = a_star(agent_idx, goal_idx, obstacles, VoronoiMap.regions)
+        except TypeError:
+            graph = CDL.create_graph(VoronoiMap.regions)
+            directions = nx.shortest_path(graph, agent_idx, goal_idx)
+                    
+        if self.show_animation:
+            print(directions)
+            self.plot(start, goal, obstacles)
+
+        return directions
+    
+    def plot(self, start, goal, obstacle):        
+        _, ax = plt.subplots()
+        
+        plt.scatter(start[0], start[1], color='green', marker='s', label='start')
+        plt.scatter(goal[0], goal[1], color='red', marker='*', label='goal')
+        
+        obstacle_x = [p.x for p in obstacle]
+        obstacle_y = [p.y for p in obstacle]
+        plt.scatter(obstacle_x, obstacle_y, color='black', marker='x', label='obstacle')
+        
+        for i, region in enumerate(VoronoiMap.regions):
+            centroid = region.centroid
+            plt.text(centroid.x, centroid.y, str(i), color='black', fontsize=10)
+            polygon = plt.Polygon(region.exterior.coords, color='blue', alpha=0.2)
+            ax.add_patch(polygon)
+        
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        
+        plt.legend()
         plt.show()

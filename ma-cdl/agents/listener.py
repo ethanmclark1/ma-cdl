3fdedf1c2679161d4cd5cab7c1a3a504503274a2
Ethan <eclark715@gmail.com>
2023-07-01 14:44:54 -0700

    Modified listener according to new VoronoiMap

--- ma-cdl/agents/listener.py
+++ ma-cdl/agents/listener.py
@@ -9,27 +9,25 @@ class Listener:
     def __init__(self, agent_radius, goal_radius, obstacle_radius, max_observable_dist):
         self.languages = ['EA', 'TD3', 'Bandit']
         self.planner = PathPlanner(agent_radius, goal_radius, obstacle_radius, max_observable_dist)
-           
+    
     def get_action(self, observation, directions, approach):
         agent_pos = observation[:2]
-        obs_pos = observation[4:-2].reshape(-1, 2)
+        obs_pos = observation[2:-2].reshape(-1, 2)
         goal_pos = observation[-2:]
         
         if approach[0] in self.languages:
             agent_region = CDL.localize(agent_pos, approach[1])
+        elif approach[0] == 'voronoi_map':
+            agent_region = CDL.localize(agent_pos, VoronoiMap.regions)
         elif approach[0] == 'grid_world':            
             agent_region = GridWorld.discretize(agent_pos)
-        elif approach[0] == 'voronoi_map':
-            agent_region = VoronoiMap.voronize(agent_pos)
         
         try:
             target_region = directions[directions.index(agent_region) + 1]
-            if approach[0] in self.languages:
+            if approach[0] in [*self.languages, 'voronoi_map']:
                 target_pos = target_region.centroid
             elif approach[0] == 'grid_world':
                 target_pos = GridWorld.dequantize(target_region)
-            elif approach[0] == 'voronoi_map':
-                target_pos = VoronoiMap.get_centroid(target_region)
         except UnboundLocalError:
             agent_idx = DirectPath.get_point_index(agent_pos)
             agent_pos = directions[agent_idx]

a4d5fe5385de32e6a4d72e9da5f64c20322564f8
Ethan <eclark715@gmail.com>
2023-09-10 22:10:51 -0700

    Updated gridworld to make n_bins consistent across entire class

--- ma-cdl/languages/baselines/grid_world.py
+++ ma-cdl/languages/baselines/grid_world.py
@@ -2,27 +2,27 @@ import numpy as np
 import networkx as nx
 
 class GridWorld:
+    n_bins = (4, 4)
     def __init__(self):
-        self.n_bins = (4, 4)
-        self.graph = nx.grid_graph(self.n_bins, periodic=False)
+        self.graph = nx.grid_graph(GridWorld.n_bins, periodic=False)
         
     # Convert continuous state to discrete state
     @staticmethod
-    def discretize(state, n_bins=(4, 4), state_ranges=((-1, 1), (-1, 1))):
+    def discretize(state, state_ranges=((-1, 1), (-1, 1))):
         discretized_state = []
         
         for i, val in enumerate(state):
-            bin_idx = np.digitize(val, np.linspace(state_ranges[i][0], state_ranges[i][1], n_bins[i] + 1)) - 1
-            discretized_state.append(min(bin_idx, n_bins[i] - 1))
+            bin_idx = np.digitize(val, np.linspace(state_ranges[i][0], state_ranges[i][1], GridWorld.n_bins[i] + 1)) - 1
+            discretized_state.append(min(bin_idx, GridWorld.n_bins[i] - 1))
         return tuple(discretized_state)
     
     # Convert discrete state to continous state for GridWorld
     @staticmethod
-    def dequantize(discretized_state, n_bins=(4, 4), state_ranges=((-1, 1), (-1, 1))):
+    def dequantize(discretized_state, state_ranges=((-1, 1), (-1, 1))):
         continuous_state = []
         
         for i, val in enumerate(discretized_state):
-            bin_width = (state_ranges[i][1] - state_ranges[i][0]) / n_bins[i]
+            bin_width = (state_ranges[i][1] - state_ranges[i][0]) / GridWorld.n_bins[i]
             continuous_val = state_ranges[i][0] + (val + 0.5) * bin_width
             continuous_state.append(continuous_val)
         return tuple(continuous_state)

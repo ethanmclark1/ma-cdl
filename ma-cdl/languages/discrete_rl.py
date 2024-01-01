Fixed bug in commutative dqn and increase reward by factor of 2

ma-cdl/languages/discrete_rl.py
@@ -226,7 +226,7 @@ class CommutativeDQN(BasicDQN):
         else:
             total_loss = torch.cat([traditional_loss.unsqueeze(-1), commutative_loss.unsqueeze(-1)])
             total_loss = torch.mean(total_loss)
-            
+        
         total_loss = self._update(total_loss)            
         return total_loss
 
@@ -243,6 +243,7 @@ class CommutativeDQN(BasicDQN):
             num_action = 0
             episode_reward = 0
 
+            language = []
             prev_action = None
             prev_reward = None
             _prev_state_proxy = []

ma-cdl/languages/utils/cdl.py
@@ -29,13 +29,13 @@ class CDL:
         self.buffer = None
         self.max_action = 8
         self.state_dims = 128
-        self.action_cost = 0.1
-        self.efficiency_factor = 0.05
+        self.action_cost = 0.125
+        self.efficiency_factor = 0
         
         self.valid_lines = set()
         self.name = self.__class__.__name__
         
-        self.configs_to_consider = 25
+        self.configs_to_consider = 40
         self.rng = np.random.default_rng(seed=42)
         self.obstacle_radius = world.large_obstacles[0].radius
     
@@ -244,7 +244,7 @@ class CDL:
         except (nx.NodeNotFound, nx.NetworkXNoPath):
             avg_safe_area = 0
             
-        return avg_safe_area
+        return 2*avg_safe_area
     
     """ 
     Calculate utility of a given problem (i.e. all configurations) 
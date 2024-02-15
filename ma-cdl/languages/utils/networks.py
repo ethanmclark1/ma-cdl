4ec77c27042af3d22e536351458c21bc18294005
Ethan <eclark715@gmail.com>
2024-02-14 21:21:38 -0700

    Removed autoencoder

--- ma-cdl/languages/utils/networks.py
+++ ma-cdl/languages/utils/networks.py
@@ -4,65 +4,16 @@ import torch.nn.functional as F
 
 from torch.optim import Adam
 
-
 torch.manual_seed(42)
-
-class Autoencoder(nn.Module):
-    def __init__(self, output_dims):
-        super(Autoencoder, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Flatten(),
-            nn.Linear(128 * 8 * 8, 512),
-            nn.ReLU(),
-            nn.Linear(512, output_dims)
-        )
-        
-        self.decoder = nn.Sequential(
-            nn.Linear(output_dims, 512),
-            nn.ReLU(),
-            nn.Linear(512, 128 * 8 * 8),
-            nn.ReLU(),
-            nn.Unflatten(1, (128, 8, 8)),
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.Sigmoid()
-        )
-
-    def forward(self, state):
-        encoded = self.encoder(state)
-        decoded = self.decoder(encoded)
-        return decoded
-    
-    def get_encoded(self, state):
-        with torch.no_grad():
-            encoded = self.encoder(state)
-        return encoded.flatten().numpy()
 
 
 class RewardEstimator(nn.Module):
     def __init__(self, input_dims, lr, step_size, gamma, dropout_rate):
         super(RewardEstimator, self).__init__()
-        self.fc1 = nn.Linear(in_features=input_dims, out_features=64)   
-        self.ln1 = nn.LayerNorm(64)
+        self.fc1 = nn.Linear(in_features=input_dims, out_features=128)   
+        self.ln1 = nn.LayerNorm(128)
         
-        self.fc2 = nn.Linear(in_features=64, out_features=16)     
+        self.fc2 = nn.Linear(in_features=128, out_features=16)     
         self.ln2 = nn.LayerNorm(16)
         
         self.fc3 = nn.Linear(in_features=16, out_features=1)

df9774b141aacefb20596556ea35fd7191dc75b1
Ethan <eclark715@gmail.com>
2024-02-19 11:02:14 -0700

    Shrunk reward estimator

--- ma-cdl/languages/utils/networks.py
+++ ma-cdl/languages/utils/networks.py
@@ -10,13 +10,13 @@ torch.manual_seed(42)
 class RewardEstimator(nn.Module):
     def __init__(self, input_dims, lr, step_size, gamma, dropout_rate):
         super(RewardEstimator, self).__init__()
-        self.fc1 = nn.Linear(in_features=input_dims, out_features=128)   
-        self.ln1 = nn.LayerNorm(128)
+        self.fc1 = nn.Linear(in_features=input_dims, out_features=64)   
+        self.ln1 = nn.LayerNorm(64)
         
-        self.fc2 = nn.Linear(in_features=128, out_features=16)     
-        self.ln2 = nn.LayerNorm(16)
+        self.fc2 = nn.Linear(in_features=64, out_features=32)     
+        self.ln2 = nn.LayerNorm(32)
         
-        self.fc3 = nn.Linear(in_features=16, out_features=1)
+        self.fc3 = nn.Linear(in_features=32, out_features=1)
         
         self.dropout = nn.Dropout(p=dropout_rate)
         self.optim = Adam(self.parameters(), lr=lr)

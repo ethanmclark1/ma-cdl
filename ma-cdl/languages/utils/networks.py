2b0729e6b2188ddb1f04c5c5b1c672a0ea645377
Ethan <eclark715@gmail.com>
2024-02-23 13:08:21 -0700

    Cleaned up reward estimator and matched td3 networks to original implementation

--- ma-cdl/languages/utils/networks.py
+++ ma-cdl/languages/utils/networks.py
@@ -8,7 +8,7 @@ torch.manual_seed(42)
 
 
 class RewardEstimator(nn.Module):
-    def __init__(self, input_dims, lr, step_size, gamma, dropout_rate):
+    def __init__(self, input_dims, lr, dropout_rate):
         super(RewardEstimator, self).__init__()
         
         fc1_output = 128 if input_dims == 63 else 64
@@ -23,7 +23,6 @@ class RewardEstimator(nn.Module):
         
         self.dropout = nn.Dropout(p=dropout_rate)
         self.optim = Adam(self.parameters(), lr=lr)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, step_size=step_size, gamma=gamma)
         
     def forward(self, x):
         x = F.relu(self.ln1(self.fc1(x)))
@@ -56,17 +55,14 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.fc1 = nn.Linear(state_dim, 256)
         self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, action_dim)
+        self.fc3 = nn.Linear(256, action_dim)
         
         self.optim = Adam(self.parameters(), lr=lr)
 
     def forward(self, state):
         a = F.relu(self.fc1(state))
         a = F.relu(self.fc2(a))
-        a = F.relu(self.fc3(a))
-        a = torch.tanh(self.fc4(a))
-        return a
+        return torch.tanh(self.fc3(a))
 
 
 class Critic(nn.Module):
@@ -76,38 +72,34 @@ class Critic(nn.Module):
         # Q1 architecture
         self.fc1 = nn.Linear(state_dim + action_dim, 256)
         self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 1)
+        self.fc3 = nn.Linear(256, 1)
 
         # Q2 architecture
-        self.fc5 = nn.Linear(state_dim + action_dim, 256)
-        self.fc6 = nn.Linear(256, 256)
-        self.fc7 = nn.Linear(256, 128)
-        self.fc8 = nn.Linear(128, 1)
+        self.fc4 = nn.Linear(state_dim + action_dim, 256)
+        self.fc5 = nn.Linear(256, 256)
+        self.fc6 = nn.Linear(256, 1)
         
         self.optim = Adam(self.parameters(), lr=lr)
 
     def forward(self, state, action):
-        xu = torch.cat([state, action], 1)
+        sa = torch.cat([state, action], 1)
 
         # Q1 architecture
-        x1 = F.relu(self.fc1(xu))
-        x1 = F.relu(self.fc2(x1))
-        x1 = F.relu(self.fc3(x1))
-        x1 = self.fc4(x1)
+        q1 = F.relu(self.fc1(sa))
+        q1 = F.relu(self.fc2(q1))
+        q1 = self.fc3(q1)
 
         # Q2 architecture
-        x2 = F.relu(self.fc5(xu))
-        x2 = F.relu(self.fc6(x2))
-        x2 = F.relu(self.fc7(x2))
-        x2 = self.fc8(x2)
-        return x1, x2
+        q2 = F.relu(self.fc4(sa))
+        q2 = F.relu(self.fc5(q2))
+        q2 = self.fc6(q2)
+        return q1, q2
 
     # More efficient to only compute Q1
-    def get_Q1(self, state, action):
-        xu = torch.cat([state, action], 1)
-        x1 = F.relu(self.fc1(xu))
-        x1 = F.relu(self.fc2(x1))
-        x1 = F.relu(self.fc3(x1))
-        x1 = self.fc4(x1)
-        return x1
+    def Q1(self, state, action):
+        sa = torch.cat([state, action], 1)
+        
+        q1 = F.relu(self.fc1(sa))
+        q1 = F.relu(self.fc2(q1))
+        q1 = self.fc3(q1)
+        return q1
